f0ef1949004f038c719ee614e0f125bfdbe1a86b
Shiry <Shiryaokv95@gmail.com>
2024-11-18 20:41:23 +0300

    Homework

---
+++ main.py
@@ -0,0 +1,14 @@
+# 1st program
+print((9 ** 0.5) * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3nd program
+num = int(2 * 2 + 2)
+print (num)
+num1 = int(2 * (2 + 2))
+print(num1)
+print(num == num1)
+#4nd program
+s1 = str('123.456')
+s1 = float(s1)
+print(int((s1 * 10) % 10))
